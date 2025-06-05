bee379dd1c14e48309190b509117d0450c588905
Desmond Obinna <talk2desmond91@gmail.com>
2025-06-05 13:35:25 +0100

    updated temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,8 +10,8 @@ def convert_to_fahrenheit(celsius):
 
 
 def main():
-    temp_input = float(input("Enter temperature to convert: "))
-    unit = input("Is this temperature in Celsius or Fahrenheit? C/F): ").strip().upper()
+    temp_input = float(input("Enter the temperature to convert:"))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F):").strip().upper()
 
     if unit == 'C':
         converted_temp = convert_to_fahrenheit(temp_input)
