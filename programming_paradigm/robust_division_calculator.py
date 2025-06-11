f1dac1e1c1ac444bdf8e0c8a374e2c84497d08f0
Desmond Obinna <talk2desmond91@gmail.com>
2025-06-11 11:14:20 +0100

    try and catch statement

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,9 @@
 def safe_divide(numerator, denominator):
     try:
         result = numerator / denominator
-        return result
+        return f'The result of the division is {result}'
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
+        return "Error: Cannot divide by zero."
 
 print(safe_divide(12,0))
 
@@ -12,8 +12,8 @@ def convert_to_floats(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
-        return numerator/denominator
+        return f'The result of the division is {numerator/denominator}'
     except ValueError:
-        return "Error: Non-numeric value provided."
+        return "Error: Please enter numeric values only."
 
 print(convert_to_floats(12, 4))
