91d16deeef76b49d3aa013d548c3f914b68af799
Desmond Obinna <talk2desmond91@gmail.com>
2025-06-11 11:47:21 +0100

    simple calculation test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,27 +2,27 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class test_simple_calc_test(unittest.TestCase):
-    def test_add(self):
-        calc = SimpleCalculator()
-        result = calc.add(5, 3)
-        self.assertEqual(result, 8)
+    def test_addition(self):
+        self.calc = SimpleCalculator()
+        self.result = self.calc.add(5, 3)
+        self.assertEqual(self.result, 8)
     
-    def test_subtract(self):
-        calc = SimpleCalculator()
-        result = calc.subtract(5, 3)
-        self.assertEqual(result, 2)
+    def test_subtraction(self):
+        self.calc = SimpleCalculator()
+        self.result = self.calc.subtract(5, 3)
+        self.assertEqual(self.result, 2)
 
     def test_multiply(self):
-        calc = SimpleCalculator()
-        result = calc.multiply(5, 3)
-        self.assertEqual(result, 15)
+        self.calc = SimpleCalculator()
+        self.result = self.calc.multiply(5, 3)
+        self.assertEqual(self.result, 15)
 
     def test_divide(self):
-        calc = SimpleCalculator()
-        result = calc.divide(6, 3)
-        self.assertEqual(result, 2.0)
+        self.calc = SimpleCalculator()
+        self.result = self.calc.divide(6, 3)
+        self.assertEqual(self.result, 2.0)
 
     def test_divide_by_zero(self):
-        calc = SimpleCalculator()
-        result = calc.divide(6, 0)
-        self.assertEqual(result, "Error: Cannot divide by zero.")
+        self.calc = SimpleCalculator()
+        self.result = self.calc.divide(6, 0)
+        self.assertEqual(self.result, "Error: Cannot divide by zero.")
