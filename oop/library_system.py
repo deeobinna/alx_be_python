ee9c0b58b7b606a904c4e7e62aee8f0f3e988534
Desmond Obinna <talk2desmond91@gmail.com>
2025-06-17 13:33:24 +0100

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,6 +8,7 @@ class Book:
 
 class EBook(Book):
     def __init__(self, file_size):
+        super().__init__(title, author) 
         self.file_size = file_size
 
     def __str__(self):
@@ -16,6 +17,7 @@ class EBook(Book):
 
 class PrintBook(Book):
     def __init__(self, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
 class Library:
