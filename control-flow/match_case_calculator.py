0f3ac3dd6967de00ac71b0d552ec6abbefea3c5b
DesmondObinna <talk2desmond91@gmail.com>
2025-11-10 12:24:40 +0100

    control flow

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation_type = input("Choose the operation (+, -, *, /)")
+operation_type = input("Choose the operation (+, -, *, /): ")
 
 
 if num2 == 0:
