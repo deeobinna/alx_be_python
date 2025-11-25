96ec3563586e6321294b4da16c9d9b5eadad76ec
DesmondObinna <talk2desmond91@gmail.com>
2025-11-25 16:10:26 +0100

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+#define a class named BankAccount
+#use the __init__ method to initialize an account_balance attribute
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = 0
+
+    #Implement deposit(amount), withdraw(amount), and display_balance() methods.
+    def deposit(self, amount):
+        #deposit should add the specified amount to account_balance.
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount >= self.account_balance:
+            print("Insufficient Balance")
+        else:
+            self.account_balance -= amount
+
+
+    def display_balance(self):
+        print(f"Account Balance: ${self.account_balance}")
