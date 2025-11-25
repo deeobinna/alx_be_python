a12a4ebb417a1b9749020a1853df48765ae882fc
DesmondObinna <talk2desmond91@gmail.com>
2025-11-25 16:15:05 +0100

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,7 +11,7 @@ class BankAccount:
 
     def withdraw(self, amount):
         if amount >= self.account_balance:
-            print("Insufficient funds")
+            print("Insufficient funds.")
         else:
             self.account_balance -= amount
 
