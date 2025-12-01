2945073eca7f3dc2ddff398d718b0c411df30408
DesmondObinna <talk2desmond91@gmail.com>
2025-12-01 11:32:38 +0100

    Book class OOP

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f'"{self.title}" by {self.author}'
+    
+class Ebook(Book):
+    def __init__(self, file_size):
+        super().__init__(self.title, self.author)
+        self.file_size = file_size  # in MB
+
+class PrintBook(Book):
+    def __init__(self, page_count):
+        super().__init__(self.title, self.author)
+        self.page_count = page_count  # in grams
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
